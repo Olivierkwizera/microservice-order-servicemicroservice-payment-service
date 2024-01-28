f1659c56a63fc7a4cf8ba1efe4271e40e2bb3896
Olivier KWIZERA <olivier.kwizera@emeal.nttdata.com>
2024-01-28 18:17:19 +0100

    Initial commit

---
+++ src/main/java/com/microservice/ps/api/service/PaymentService.java
@@ -0,0 +1,28 @@
+package com.microservice.ps.api.service;
+
+import com.microservice.ps.api.entity.Payment;
+import com.microservice.ps.api.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+import java.util.UUID;
+
+@Service
+public class PaymentService {
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    public Payment doPayment(Payment payment) {
+        payment.setPaymentStatus(paymentProcessing());
+        payment.setTransactionId(UUID.randomUUID().toString());
+        return paymentRepository.save(payment);
+    }
+    public String paymentProcessing(){
+        return  new Random().nextBoolean()? "success" : "fail";
+    }
+
+    public Payment findPaymentHistoryByOrderId(int payementOrderId) {
+        return paymentRepository.findByOrderId(payementOrderId);
+    }
+}
